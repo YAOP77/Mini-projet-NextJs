0f18fc33e87ccc08ed5964422c398928791e48c7
YAOP77 <yaop77@gmail.com>
2025-09-29 14:51:56 +0000

    Suppression clé API du next.config.js pour sécurité Vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_TMDB_API_KEY: '8c247ea0b4b56ed2ff7d41c9a833aa77',
-  },
+  // env supprimé, la clé TMDB doit être définie dans les variables d'environnement Vercel
   images: {
     domains: ['image.tmdb.org'],
   },
